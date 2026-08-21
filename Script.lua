883762025d375219b8df8ecb51f6a52ffa58c820
m23660314-stack <m23660314@gmail.com>
2026-08-21 19:58:28 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,3 +1,8 @@
-print("hello bro !!!!")
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.Players.LocalPlayer.PlayerGui
 
-local part = inctance.new("Part")
+local button = Instance.new("TextButton")
+button.Size = UDim2.new(0, 200, 0, 50)
+button.Position = UDim2.new(0.5, -100, 0.5, -25)
+button.Text = "CLICK ME"
+button.Parent = gui
