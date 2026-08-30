280cad3f77b21ff2de1435ee0c81d0539cde62da
m23660314-stack <m23660314@gmail.com>
2026-08-30 22:17:10 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -56,3 +56,17 @@ local Button = MainTab:CreateButton({
 		game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid").Health = 0
 	end,
 })
+
+
+local Slider = MainTab:CreateSlider({
+	Name = "Walkspeed",
+	Range = {0, 500},
+	Increment = 1,
+	Suffix = " Speed",
+	CurrentValue = 16,
+	Flag = "Slider1", -- A flag is the identifier for the configuration file; make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Value)
+		
+		game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid").WalkSpeed = Value
+	end,
+})
